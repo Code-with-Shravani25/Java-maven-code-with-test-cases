d9ac38d9db20dbcb9e8b372d77a5f8e3d3aebfd7
Shravani Budharam <shravanibudharam25@gmail.com>
2026-05-31 23:45:44 +0530

    Create CalculatorTest.java

---
+++ src/test/java/com/demo/CalculatorTest.java
@@ -0,0 +1,24 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    void testAdd() {
+        assertEquals(10, calculator.add(5,5));
+    }
+
+    @Test
+    void testSubtract() {
+        assertEquals(2, calculator.subtract(5,3));
+    }
+
+    @Test
+    void testMultiply() {
+        assertEquals(15, calculator.multiply(3,5));
+    }
+}
